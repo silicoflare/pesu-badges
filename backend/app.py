c7f9837066fc033a53d3dbf5e664a1fcf28b8c65
Suraj B M <silicoflare@gmail.com>
2023-10-15 08:20:23 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -17,11 +17,10 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],  # You can specify the HTTP methods that are allowed
-    allow_headers=["*"],  # You can specify the HTTP headers that are allowed
+    allow_methods=["*"],  
+    allow_headers=["*"],  
 )
 
-# app.mount("/", StaticFiles(directory="./frontend/dist", html=True))
 app.mount("/static", StaticFiles(directory="./static"), name="static")
 
 
@@ -41,31 +40,4 @@ def list_files():
 def get_svg(file_name: str = Path(..., title="The name of your SVG file")):
     file_path = f"./static/{file_name}.svg"
     return FileResponse(file_path, media_type="image/svg+xml")
-
-def generate_custom_font_badge(version):
-    dwg = Drawing()
-
-    # Badge style
-    dwg.add(dwg.rect(insert=(0, 0), size=((len(version)+2)*9, 20), rx=5, ry=5, fill="#4c1"))
-
-    # Custom font style
-    font_size = "12px"
-    font_weight = "bold"
-
-    # Include the font style from the URL
-    custom_font_style = f"""
-        @import url('https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap');
-        text {{
-            font-family: 'Source Code Pro', monospace;
-            font-size: {font_size};
-            font-weight: {font_weight};
-            fill: white;
-        }}
-    """
-    dwg.add(dwg.style(custom_font_style))
-
-    # Badge text with custom font
-    dwg.add(dwg.text(version, insert=(12, 14)))
-
-    return dwg.tostring()
 
