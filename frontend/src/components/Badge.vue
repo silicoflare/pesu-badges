763260358131d17c533817e3cc911b64505fd4e9
Suraj B M <silicoflare@gmail.com>
2023-10-15 08:12:10 +0530

    Modify copy function

--- frontend/src/components/Badge.vue
+++ frontend/src/components/Badge.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="w-[15rem] h-[15rem] m-auto p-5 text-2xl flex flex-col justify-center items-center border-2 border-black" 
-    @click="navigator.clipboard.writeText(`<img src='https://pesu-badges-api.vercel.app/badge/${name}' width='98px' height='30px' />`)">
+    @click="copy">
         <img :src='`https://pesu-badges-api.vercel.app/badge/${name}`' width="98px" height="30px" class="w-max h-auto"/><br>
         <span>{{ name }}</span>
     </div>
@@ -8,6 +8,16 @@
 
 <script>
     export default {
-        props: ['name']
+        props: ['name'],
+        methods: {
+            copy()  {
+                const string = `<img src='https://pesu-badges-api.vercel.app/badge/${this.name}' width='98px' height='30px' />`;
+                console.log(string);
+                navigator.clipboard.writeText(string)
+                .then(() => {
+                    alert("Copied successfully!");
+                });
+            }
+        }
     }
 </script>
